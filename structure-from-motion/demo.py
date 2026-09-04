d6b59a5d682bf5b079469376ce620970f41d50ae
lohjc <jiachiewloh@gmail.com>
2026-09-05 01:55:32 +0800

    rename var name and refractor

--- structure-from-motion/demo.py
+++ structure-from-motion/demo.py
@@ -59,8 +59,8 @@ def auto_select_pose_and_points(
     # ==========================================
     try:
         E_mat2, mask2 = cv.findEssentialMat(
-            points1=pts1_norm,
-            points2=pts2_norm,
+            pts_1=pts1_norm,
+            pts_2=pts2_norm,
             cameraMatrix=np.eye(3),
             method=cv.RANSAC,
             prob=0.99,
@@ -171,15 +171,15 @@ def match(img_left, img_right):
     sift = cv.SIFT_create()
 
     # Detect keypoints and compute descriptors
-    kp_left, des_left = sift.detectAndCompute(img_left, None)
-    kp_right, des_right = sift.detectAndCompute(img_right, None)
+    kp_left, desc_left = sift.detectAndCompute(img_left, None)
+    kp_right, desc_right = sift.detectAndCompute(img_right, None)
 
     # Use FLANN-based matcher
     index_params = dict(algorithm=1, trees=5)  # Using KDTree for SIFT
     search_params = dict(checks=50)
     flann = cv.FlannBasedMatcher(index_params, search_params)
 
-    matches = flann.knnMatch(des_left, des_right, k=2)
+    matches = flann.knnMatch(desc_left, desc_right, k=2)
 
     # Apply Lowe's ratio test
     good_matches = []
@@ -187,7 +187,82 @@ def match(img_left, img_right):
         if m.distance < MATCHING_THRESHOLD * n.distance:
             good_matches.append(m)
 
-    return kp_left, kp_right, good_matches
+    return kp_left, kp_right, desc_left, desc_right, good_matches
+
+
+# this is use to track 3d points based on im0 (img with idx 0)
+# key im0  -> value = x,y,z coord
+point_cloud_tracker = {}
+
+
+def init_3d_points(img1, img2):
+    gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
+    gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
+
+    kp_1, kp_2, desc_1, desc_2, good_matches = match(gray1, gray2)
+
+    pts_1 = np.float32([kp_1[m.queryIdx].pt for m in good_matches])
+    pts_2 = np.float32([kp_2[m.trainIdx].pt for m in good_matches])
+    print(f"pts_1: {len(pts_1)}")
+    print(f"pts_2: {len(pts_2)}")
+
+    # draw matches
+    draw_match = True
+    if draw_match:
+        img_matches = cv.drawMatches(
+            img1,
+            kp_1,
+            img2,
+            kp_2,
+            good_matches,
+            None,
+            flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+        )
+        cv.namedWindow("Matches", cv.WINDOW_NORMAL)
+        cv.imshow("Matches", img_matches)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+
+    # find matrices
+    pts1_norm = cv.undistortPoints(
+        np.expand_dims(pts_1, axis=1),
+        cameraMatrix=K,
+        distCoeffs=None,
+    )
+    pts2_norm = cv.undistortPoints(
+        np.expand_dims(pts_2, axis=1),
+        cameraMatrix=K,
+        distCoeffs=None,
+    )
+    E_mat, R, t, points_3d, chosen_method = auto_select_pose_and_points(
+        pts_1, pts_2, pts1_norm, pts2_norm, K, K
+    )
+
+    print(f"Essential Matrix:\n{E_mat}")
+    print(f"Rotation Matrix (R):\n{R}")
+    print(f"Translation Vector (t):\n{t}")
+
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection="3d")
+
+    # Scatter plot configuration
+    # X = Column 0, Y = Column 1, Z = Column 2
+    ax.scatter(
+        points_3d[:, 0],
+        points_3d[:, 1],
+        points_3d[:, 2],
+        c=points_3d[:, 2],
+    )
+
+    ax.set_xlabel("X Axis")
+    ax.set_ylabel("Y Axis")
+    ax.set_zlabel("Z Axis")
+    ax.set_title("Matplotlib 3D Points Visualization")
+
+    # Set initial viewing angle perspective
+    ax.view_init(elev=-90, azim=-90)
+
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -197,83 +272,12 @@ if __name__ == "__main__":
         img = cv.imread(imgpath)
         imgs.append(img)
 
-    for i in range(len(imgs) - 1):
-        img1 = imgs[i].copy()
-        img2 = imgs[i + 1].copy()
-
-        kp_img1 = img1.copy()
-        kp_img2 = img2.copy()
-
-        gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
-        gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
-
-        kp1, kp2, good_matches = match(gray1, gray2)
-
-        points1 = np.float32([kp1[m.queryIdx].pt for m in good_matches])
-        points2 = np.float32([kp2[m.trainIdx].pt for m in good_matches])
-        print(f"points1: {len(points1)}")
-        print(f"points2: {len(points2)}")
-
-        # draw matches
-        draw_match = True
-        if draw_match:
-            img_matches = cv.drawMatches(
-                img1,
-                kp1,
-                img2,
-                kp2,
-                good_matches,
-                None,
-                flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
-            )
-            cv.namedWindow("Matches", cv.WINDOW_NORMAL)
-            cv.imshow("Matches", img_matches)
-            cv.waitKey(0)
-            cv.destroyAllWindows()
-
-        # find matrices
-        use_method = 2
-        pts1_norm = cv.undistortPoints(
-            np.expand_dims(points1, axis=1),
-            cameraMatrix=K,
-            distCoeffs=None,
-        )
-        pts2_norm = cv.undistortPoints(
-            np.expand_dims(points2, axis=1),
-            cameraMatrix=K,
-            distCoeffs=None,
-        )
-        E_mat, R, t, points_3D, chosen_method = auto_select_pose_and_points(
-            points1, points2, pts1_norm, pts2_norm, K, K
-        )
-
-        print(f"Essential Matrix:\n{E_mat}")
-        print(f"Rotation Matrix (R):\n{R}")
-        print(f"Translation Vector (t):\n{t}")
-
-        # triangulate 3D points
-        points_3d = triangulate_points(points1, points2, K, K, R, t)
-
-        fig = plt.figure(figsize=(10, 8))
-        ax = fig.add_subplot(111, projection="3d")
-
-        # Scatter plot configuration
-        # X = Column 0, Y = Column 1, Z = Column 2
-        ax.scatter(
-            points_3d[:, 0],
-            points_3d[:, 1],
-            points_3d[:, 2],
-            c=points_3d[:, 2],
-        )
-
-        ax.set_xlabel("X Axis")
-        ax.set_ylabel("Y Axis")
-        ax.set_zlabel("Z Axis")
-        ax.set_title("Matplotlib 3D Points Visualization")
-
-        # Set initial viewing angle perspective
-        ax.view_init(elev=-90, azim=-90)
-
-        plt.show()
-
-        break  # debug use: temp using 1st 2 images
+    for i in range(len(imgs)):
+        if i <= 1:
+            if i == 0:
+                img1 = imgs[i].copy()
+                img2 = imgs[i + 1].copy()
+                init_3d_points(img1, img2)
+        else:
+            # solve pnp
+            pass
