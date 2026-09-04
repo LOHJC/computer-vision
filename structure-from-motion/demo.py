a3840caadb8296c1e1d4f9031d1e4775ca2a0cd7
lohjc <jiachiewloh@gmail.com>
2026-09-05 00:01:21 +0800

    clean up demo matching code and update readme

--- structure-from-motion/demo.py
+++ structure-from-motion/demo.py
@@ -1,22 +1,38 @@
-
 import cv2 as cv
 import os
 import matplotlib.pyplot as plt
 import numpy as np
-import open3d as o3d
 
-IMG_PATH = "./imgs"
+IMG_PATH = "./viking"
+K = np.matrix("523.81 0.00 252.00; 0.00 523.81 336.00; 0.00 0.00 1.00")
 
-def fill_image(img):
-    """Find the biggest contour and create a mask"""
-    contours, _ = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-    if not contours:
-        return img
-    
-    biggest_contour = max(contours, key=cv.contourArea)
-    mask = np.zeros_like(img)
-    cv.drawContours(mask, [biggest_contour], 0, 255, -1)
-    return mask
+
+MATCHING_THRESHOLD = 0.5  # 0.7
+
+
+def match(img_left, img_right):
+    # Initialize SIFT detector
+    sift = cv.SIFT_create()
+
+    # Detect keypoints and compute descriptors
+    kp_left, des_left = sift.detectAndCompute(img_left, None)
+    kp_right, des_right = sift.detectAndCompute(img_right, None)
+
+    # Use FLANN-based matcher
+    index_params = dict(algorithm=1, trees=5)  # Using KDTree for SIFT
+    search_params = dict(checks=50)
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+
+    matches = flann.knnMatch(des_left, des_right, k=2)
+
+    # Apply Lowe's ratio test
+    good_matches = []
+    for m, n in matches:
+        if m.distance < MATCHING_THRESHOLD * n.distance:
+            good_matches.append(m)
+
+    return kp_left, kp_right, good_matches
+
 
 if __name__ == "__main__":
     imgs = []
@@ -24,14 +40,8 @@ if __name__ == "__main__":
         imgpath = os.path.join(IMG_PATH, imgname)
         img = cv.imread(imgpath)
         imgs.append(img)
-    
-    feature_detector = cv.SIFT.create()
-    matcher = cv.BFMatcher()
 
-    # feature_detector = cv.ORB.create()
-    # matcher = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
-    
-    for i in range(len(imgs)-1):
+    for i in range(len(imgs) - 1):
         img1 = imgs[i].copy()
         img2 = imgs[i + 1].copy()
 
@@ -41,46 +51,28 @@ if __name__ == "__main__":
         gray1 = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
         gray2 = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
 
-        # rough remove background
-        _, bin1 = cv.threshold(gray1, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
-        bin1 = fill_image(bin1)
-        _, bin2 = cv.threshold(gray2, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
-        bin2 = fill_image(bin2)
+        kp1, kp2, good_matches = match(gray1, gray2)
 
-        cv.bitwise_and(gray1, bin1, gray1)
-        cv.bitwise_and(gray2, bin2, gray2)
+        break  # debug use: temp using 1st 2 images
 
-        # plt.subplot(1,2,1)
-        # plt.imshow(gray1)
-        # plt.subplot(1,2,2)
-        # plt.imshow(gray2)
-        # plt.show()
+    points1 = np.float32([kp1[m.queryIdx].pt for m in good_matches])
+    points2 = np.float32([kp2[m.trainIdx].pt for m in good_matches])
+    print(f"points1: {len(points1)}")
+    print(f"points2: {len(points2)}")
 
-        kp1, descriptor1 = feature_detector.detectAndCompute(gray1, None)
-        kp2, descriptor2 = feature_detector.detectAndCompute(gray2, None)
-
-        cv.drawKeypoints(kp_img1, kp1, kp_img1)
-        cv.drawKeypoints(kp_img2, kp2, kp_img2)
-
-        # sift
-        matches = matcher.knnMatch(descriptor1, descriptor2, k=2)
-        good_matches = []
-        for m, n in matches:
-            if m.distance < 0.7 * n.distance:
-                good_matches.append([m])
-        match_res = cv.drawMatchesKnn(img1, kp1, img2, kp2, good_matches, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        
-        # orb
-        # matches = matcher.match(descriptor1, descriptor2)
-        # matches = sorted(matches, key = lambda x:x.distance)
-        # match_res = cv.drawMatches(img1, kp1, img2, kp2, matches, None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
-        plt.subplot(1,3,1)
-        plt.imshow(img1)
-        plt.subplot(1,3,2)
-        plt.imshow(img2)
-        plt.subplot(1,3,3)
-        plt.imshow(match_res)
-        plt.show()
-
-        break
+    # draw matches
+    draw_match = True
+    if draw_match:
+        img_matches = cv.drawMatches(
+            img1,
+            kp1,
+            img2,
+            kp2,
+            good_matches,
+            None,
+            flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+        )
+        cv.namedWindow("Matches", cv.WINDOW_NORMAL)
+        cv.imshow("Matches", img_matches)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
--- structure-from-motion/readme.md
+++ structure-from-motion/readme.md
@@ -4,15 +4,15 @@
 - https://www.youtube.com/watch?v=iJTqlb7gsWY&t
 - https://github.com/openMVG/SfM_quality_evaluation/tree/master
 - https://github.com/muneebaadil/structure-from-motion/blob/master/README.md
+- https://colmap.github.io/index.html
 
 ## datasets
 - ~~https://www.eth3d.net/datasets~~
 - https://demuc.de/colmap/datasets/
-  - south building (smallest set provided)
+  - image set using: south building (smallest set provided)
   - camera params are provided in `cameras.txt` and `images.txt` in `south-building/sparse`
-  - 
 - https://github.com/rshilliday/sfm/tree/master/datasets/Viking
-  - image set using: Viking 
+  - image set using: viking
   - requires to get the camera param in the code `https://github.com/rshilliday/sfm/blob/master/matching.py`
     - `K = np.matrix('523.81 0.00 252.00; 0.00 523.81 336.00; 0.00 0.00 1.00')`
   - can download the dataset via `https://download-directory.github.io/`
